211f5f222d22c2d12c3d6c6f0819e7671c2f50a1
User <User@DESKTOP-9O1GBIC>
2022-01-18 00:04:37 +0200

    Добавьте файлы проекта.

---
+++ lab1/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label_ok_2 = new System.Windows.Forms.Label();
+            this.label_ok_1 = new System.Windows.Forms.Label();
+            this.button1_OK = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.textBox_login = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label_ok_2);
+            this.groupBox1.Controls.Add(this.label_ok_1);
+            this.groupBox1.Controls.Add(this.button1_OK);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox_password);
+            this.groupBox1.Controls.Add(this.textBox_login);
+            this.groupBox1.Location = new System.Drawing.Point(68, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(331, 152);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Авторизація";
+            // 
+            // label_ok_2
+            // 
+            this.label_ok_2.AutoSize = true;
+            this.label_ok_2.Location = new System.Drawing.Point(237, 88);
+            this.label_ok_2.Name = "label_ok_2";
+            this.label_ok_2.Size = new System.Drawing.Size(19, 13);
+            this.label_ok_2.TabIndex = 6;
+            this.label_ok_2.Text = "ok";
+            // 
+            // label_ok_1
+            // 
+            this.label_ok_1.AutoSize = true;
+            this.label_ok_1.Location = new System.Drawing.Point(234, 50);
+            this.label_ok_1.Name = "label_ok_1";
+            this.label_ok_1.Size = new System.Drawing.Size(19, 13);
+            this.label_ok_1.TabIndex = 5;
+            this.label_ok_1.Text = "ok";
+            // 
+            // button1_OK
+            // 
+            this.button1_OK.Location = new System.Drawing.Point(106, 123);
+            this.button1_OK.Name = "button1_OK";
+            this.button1_OK.Size = new System.Drawing.Size(75, 23);
+            this.button1_OK.TabIndex = 4;
+            this.button1_OK.Text = "ok";
+            this.button1_OK.UseVisualStyleBackColor = true;
+            this.button1_OK.Click += new System.EventHandler(this.button1_OK_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "login";
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Location = new System.Drawing.Point(106, 81);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(100, 20);
+            this.textBox_password.TabIndex = 1;
+            // 
+            // textBox_login
+            // 
+            this.textBox_login.Location = new System.Drawing.Point(106, 44);
+            this.textBox_login.Name = "textBox_login";
+            this.textBox_login.Size = new System.Drawing.Size(100, 20);
+            this.textBox_login.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 184);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1_OK;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.TextBox textBox_login;
+        private System.Windows.Forms.Label label_ok_2;
+        private System.Windows.Forms.Label label_ok_1;
+    }
+}
+
---
+++ lab1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_OK_Click(object sender, EventArgs e)
+        {
+            if (textBox_login.Text == "Kate")
+                label_ok_1.BackColor = Color.Green;
+            else
+                label_ok_1.BackColor = Color.Red;
+            if (textBox_password.Text == "1234")
+                label_ok_2.BackColor = Color.Green;
+            else
+                label_ok_2.BackColor = Color.Red;
+
+            if (textBox_login.Text == "Kate" && textBox_password.Text == "1234")
+            {
+
+
+                MessageBox.Show("Hello, Kate");
+            }
+            else
+            {
+
+
+                MessageBox.Show("Error login or passwrod");
+            }
+        }
+    }
+}
+
+
+
+
